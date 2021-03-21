17231a0a1b613c915710f8b925ca117d1118685f
carinalsilverio <carinalsilverio@users.noreply.github.com>
2021-03-21 14:43:50 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from  'react'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
 
 function App() {
   return (
     <div>
-      App React
+        <Header />
+        <Footer />
     </div>
   );
 }
